fe9889a8f45b8b49a35f6f04cb178c8fa3d903d5
Svetlana Pavlova <>
2020-08-30 12:15:12 +0300

    Add test class

--- src/test/java/AllureTests.java
+++ src/test/java/AllureTests.java
@@ -1,9 +1,11 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class AllureTests {
 
     @Test
     void dksk(){
+        Assertions.assertTrue(false);
 
     }
 
