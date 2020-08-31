45729406a2e16bf46c9fa6a0a468e9f3cab18901
kefir <lala@mail.ru>
2020-09-01 00:39:46 +0300

    add config and updte class for tests

--- src/test/java/AllureTests.java
+++ src/test/java/AllureTests.java
@@ -6,8 +6,8 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class AllureTests {
 
-    private final String LOGIN = "ririannivel@gmail.com";
-    private final String PASSWORD = "ririannivel1"; //TODO fake account's password should be changed to secret password
+    private final String LOGIN = "dimon9210@mail.ru";
+    private final String PASSWORD = ""; //TODO fake account's password should be changed to secret password
     private final String TITLE = "Loading all photos from an instagram account";
 
     @Test
@@ -16,8 +16,7 @@ public class AllureTests {
         $("a[href='/login']").click();
         $("input[name='login']").sendKeys(LOGIN);
         $("input[name='password']").sendKeys(PASSWORD);
-//        $("input[name='commit']").click();
-        Assertions.assertTrue(false);
+        $("input[name='commit']").click();
 
     }
 
